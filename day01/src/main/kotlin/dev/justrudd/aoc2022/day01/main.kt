fcc97c30c8972c1cad3b43d4d566cbabf5943c9c
Justin Rudd <justinrudd@helpscout.com>
2022-12-09 14:47:58 -0800

    Implement Day 1

--- day01/src/main/kotlin/dev/justrudd/aoc2022/day01/main.kt
+++ day01/src/main/kotlin/dev/justrudd/aoc2022/day01/main.kt
@@ -1,5 +1,39 @@
 package dev.justrudd.aoc2022.day01
 
+import java.io.File
+import kotlin.text.Charsets.US_ASCII
+
+@JvmInline
+value class Food(val calories: Long)
+
+data class Elf(
+    val id: Int,
+    val foods: MutableList<Food> = mutableListOf()
+    ) {
+
+    fun totalCalories() =
+        foods.sumOf { it.calories }
+
+}
+
 fun main() {
-    println("Hello")
+    val elves = mutableListOf<Elf>()
+    elves += Elf(1)
+    File("day01/input.txt").forEachLine(US_ASCII) { line ->
+        if (line.isBlank()) {
+            elves += Elf(elves.last().id + 1)
+        } else {
+            elves.last().foods += Food(line.toLong())
+        }
+    }
+
+    val elf = elves.maxBy { it.totalCalories() }
+    println("Part One - ${elf.totalCalories()}")
+
+    val caloriesOfTop3 =
+        elves
+            .sortedByDescending { it.totalCalories() }
+            .take(3)
+            .sumOf { it.totalCalories() }
+    println("Part Two - $caloriesOfTop3")
 }
